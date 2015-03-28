67c58a8d5a93bda9e7063edbc52fed9e38c604c0
damienbod <damien_bod@hotmail.com>
2015-03-28 15:04:43 +0100

    clean up

--- src/AspNet5AngularJSDynamicRoutes/gruntfile.js
+++ src/AspNet5AngularJSDynamicRoutes/gruntfile.js
@@ -1,9 +1,5 @@
-// This file in the main entry point for defining grunt tasks and using grunt plugins.
-// Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
-
 module.exports = function (grunt) {
 
-	// load Grunt plugins from NPM
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
@@ -30,13 +26,8 @@ module.exports = function (grunt) {
 		}
 	});
 
-	// This command registers the default task which will install bower packages into wwwroot/lib
 	grunt.registerTask("default", ["bower:install"]);
-
-	// define tasks
 	grunt.registerTask('default', ['uglify', 'watch']);
 
-	// The following line loads the grunt plugins.
-	// This line needs to be at the end of this this file.
 	grunt.loadNpmTasks("grunt-bower-task");
 };
